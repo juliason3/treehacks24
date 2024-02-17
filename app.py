d16fb8433e86c24c29d2c2166aecb7767f90fbdf
juliason3 <juliason3@g.ucla.edu>
2024-02-17 14:22:16 -0800

    added config.py, instance folder, migrations folder (for flask), and *should have* set up database tables for connection with SQLite

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import SQLALCHEMY_DATABASE_URI
+
 app = Flask(__name__)
-@app.route('/')
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+#app.config.from_pyfile('config.py') #load configuration from config.py
+
+db = SQLAlchemy(app) #initialize Flask-SQLAlchemy
+migrate = Migrate(app, db)
+
+#defining database models
+#creating a simple user model:
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique = True, nullable = False)
+    email = db.Column(db.String(120), unique = True, nullable = False)
+
+    def __repr__(self): 
+        return '<User %r>' %self.username #returning string with username
+
+@app.route('/') 
 def hello_world():    
     return 'Hello, World!'  
 if __name__ == '__main__':     
---
+++ config.py
@@ -0,0 +1,6 @@
+#config.py
+
+#configuration settings for Flask app
+#Database URI for SQLite
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db' #name of SQLite database file is app.db
