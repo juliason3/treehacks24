f43b700cc7c9e879426f84589b2a62b6a4d1d0ec
Kylie Bach <kyliebach@Kylies-MacBook-Air.local>
2024-02-17 14:24:10 -0800

    deleted treehacks24 extra folder

--- treehacks24-working/flask-backend/app.py
+++ treehacks24-working/flask-backend/app.py
@@ -1,7 +1,24 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+CORS(app)  # This will enable CORS for all routes and origins.
+import pytesseract
+from PIL import Image
+import io
+
 app = Flask(__name__)
-@app.route('/')
-def hello_world():    
-    return 'Hello, World!'  
-if __name__ == '__main__':     
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return jsonify(error="No file part"), 400
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify(error="No selected file"), 400
+    if file:
+        # Read the image via file.stream
+        img = Image.open(io.BytesIO(file.read()))
+        text = pytesseract.image_to_string(img)
+        return jsonify(text=text), 200
+
+if __name__ == '__main__':
     app.run(debug=True)
